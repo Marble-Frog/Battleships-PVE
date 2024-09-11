dda8c9fe9b7f3903222cd484b8696c211c1c6908
Marble-Frog <kadesamboateng@googlemail.com>
2024-09-11 15:57:59 +0100

    Cleaned up print_board syntax

--- run.py
+++ run.py
@@ -15,11 +15,11 @@ letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H
 
 
 def print_board(board):
-    print('  A B C D E F H')
-    print(' --------------')
+    print('  A B C D E F G H')
+    print(' ---------------')
     row_number = 1
     for row in board:
-        print("%d|%s" % (row_number, "|".join(row)))
+        print(f"{row_number}|{'|'.join(row)}")
         row_number += 1
         #For each row iterated a seporator is added 
 
